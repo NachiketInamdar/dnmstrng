7fff02c2f769cd8d28ebda2481daa2061da8106e
Nachiket Inamdar <ni_inamdar@yahoo.com>
2024-02-06 15:10:29 +0530

    Uploaded console app.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var counter = 0;
+        var max = args.Length is not 0 ? Convert.ToInt32(args[0]) : -1;
+        while(max is -1 || counter < max)
+        {
+            Console.WriteLine($"Counter: {++counter}");
+            await Task.Delay(TimeSpan.FromMilliseconds(1_000));
+        }
+    }
+}
